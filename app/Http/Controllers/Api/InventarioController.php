d341e0ef4b647c80479eb523797cae44e5e6cbae
Marcos Sanzana <33297167+msanzana@users.noreply.github.com>
2024-06-23 00:02:59 -0400

    adelanto fix inventarios

--- app/Http/Controllers/Api/InventarioController.php
+++ app/Http/Controllers/Api/InventarioController.php
@@ -10,27 +10,28 @@ class InventarioController extends Controller
 {
     public function index(Request $request)
     {
+        $consulta = null;
 
         if($request->tipo_filtro == 1)
         {
             $consulta = DB::table('kardex AS kard')
-                            ->join('productos AS prod','prod','prod.id','=','kard.producto_id')
+                            ->join('productos AS prod','prod.id','=','kard.producto_id')
                             ->join('sucursales AS sucu','sucu.id','=','kard.sucursal_id')
                             ->select('kard.id AS id',
-                                     'kard.fecha AS fecha',
-                                     'kardex.producto_id AS producto_id',
-                                     'pro.nombre AS producto',
-                                     DB::raw('(SELECT IFNULL(SUM(kar.nueva_cantidad),0) FROM kardex AS kar WHERE kar.fecha = '.$request->fecha.' AND kar.producto_id = kard.id AND kar.modulo_id = 1) AS entradas'),
-                                     DB::raw('(SELECT IFNULL(SUM(kar.nueva_cantidad),0) FROM kardex AS kar WHERE kar.fecha = '.$request->fecha.' AND kar.producto_id = kard.id AND kar.modulo_id = 2) AS salidas'),
-                                     DB::raw('(
-                                                  (SELECT IFNULL(SUM(kar.nueva_cantidad),0) FROM kardex AS kar WHERE kar.fecha = '.$request->fecha.' AND kar.producto_id = kard.id AND kar.modulo_id = 1) -
-                                                  (SELECT IFNULL(SUM(kar.nueva_cantidad),0) FROM kardex AS kar WHERE kar.fecha = '.$request->fecha.' AND kar.producto_id = kard.id AND kar.modulo_id = 2)
-                                              ) AS saldo'))
-                            ->whereIN('kard.sucursal_id',$request->sucursal)
-                            ->where('kard.fecha','=',$request->fecha)
-                            ->groupBy('kard.fecha')
+                                     DB::raw('DAY(kard.fecha) AS dia'),
+                                     DB::raw('MONTH(kard.fecha) AS mes'),
+                                     DB::raw('YEAR(kard.fecha) AS anyo'),
+                                     'kard.producto_id AS producto_id',
+                                     'prod.nombre AS producto',
+                                     DB::raw('IFNULL((SELECT SUM(kar.nueva_cantidad) FROM kardex AS kar WHERE DATE(kar.fecha) = "'.$request->fecha.'" AND kar.producto_id = kard.producto_id AND kar.modulo_id = 1 GROUP BY kar.fecha),0) AS entradas'),
+                                     DB::raw('IFNULL((SELECT SUM(kar.nueva_cantidad) FROM kardex AS kar WHERE DATE(kar.fecha) = "'.$request->fecha.'" AND kar.producto_id = kard.producto_id AND kar.modulo_id = 2 GROUP BY kar.fecha),0) AS salidas'),
+                                     DB::raw('(IFNULL((SELECT SUM(kar.nueva_cantidad) FROM kardex AS kar WHERE DATE(kar.fecha) = "'.$request->fecha.'" AND kar.producto_id = kard.producto_id AND kar.modulo_id = 1 GROUP BY kar.fecha),0) -
+                                               IFNULL((SELECT SUM(kar.nueva_cantidad) FROM kardex AS kar WHERE DATE(kar.fecha) = "'.$request->fecha.'" AND kar.producto_id = kard.producto_id AND kar.modulo_id = 2 GROUP BY kar.fecha),0)) AS saldo'))
+                            ->whereIn('kard.sucursal_id',$request->sucursal)
+                            ->whereRaw('DATE(kard.fecha) = "'.$request->fecha.'"')
+                            ->groupBy('kard.fecha','kard.producto_id')
                             ->get();
-
+           error_log(json_encode($consulta));
         }
         else if($request->tipo_filtro == 2)
         {
@@ -73,7 +74,6 @@ class InventarioController extends Controller
                             ->groupBy(DB::rae('YEAR(kard.fecha)'))
                             ->get();
         }
-
         return $consulta;
     }
 }
--- config/database.php
+++ config/database.php
@@ -56,6 +56,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=(SELECT REPLACE(@@sql_mode, "ONLY_FULL_GROUP_BY", ""))',
             ]) : [],
         ],
 
@@ -76,6 +77,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=(SELECT REPLACE(@@sql_mode, "ONLY_FULL_GROUP_BY", ""))',
             ]) : [],
         ],
 
